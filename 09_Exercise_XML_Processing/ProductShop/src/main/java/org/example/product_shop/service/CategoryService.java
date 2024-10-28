f5eb4550b1be706d8d6d662f3230458bfa3d80eb
Ivan Dachev <dacheww13@gmail.com>
2024-10-28 21:18:56 +0200

    Delete 09_Exercise_XML_Processing directory

--- 09_Exercise_XML_Processing/ProductShop/src/main/java/org/example/product_shop/service/CategoryService.java
+++
@@ -1,9 +0,0 @@
-package org.example.product_shop.service;
-
-import java.io.FileNotFoundException;
-
-public interface CategoryService {
-    void seedCategories() throws Exception;
-
-    void exportCategoriesByProductStats() throws Exception;
-}
--- 09_Exercise_XML_Processing/ProductShop/src/main/java/org/example/product_shop/service/ProductService.java
+++
@@ -1,9 +0,0 @@
-package org.example.product_shop.service;
-
-import java.math.BigDecimal;
-
-public interface ProductService {
-    void seedProducts() throws Exception;
-
-    void exportProductsBetweenPricesWithoutBuyer(BigDecimal low, BigDecimal high) throws Exception;
-}
